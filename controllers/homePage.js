8e98926a76933892ec301ec625e5876e42379824
Dilshodbek <dilshodjongulomov53@gmail.com>
2023-09-13 23:44:27 +0500

    make it better

--- controllers/homePage.js
+++ controllers/homePage.js
@@ -1,8 +1,8 @@
 const Post = require("../models/post");
 
 module.exports = async (req, res) => {
-  console.log(req.session);
+  // console.log(req.session);
   const posts = await Post.find().populate("author")
-  console.log(posts);
+  // console.log(posts);
   res.render("index", { posts });
 };
